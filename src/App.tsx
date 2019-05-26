309a6acb2d53dd5c94b047b264b73e37b1ebbae6
Matsuda Yuichiro <Yuichiro@Matsuda-MBP.local>
2019-05-27 02:04:02 +0900

    :+1: redux setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 import * as React from 'react';
+import { Route, Switch } from 'react-router';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import { AppShell } from './AppShell';
+import { browserHistory, store } from './store';
 
-export const App = () => {
-  return <div>Atomic Design with Redux</div>
-};
+export const App = () => (
+  <Provider store={store}>
+    <AppShell>
+      <ConnectedRouter history={browserHistory}>
+        <Switch>
+          <Route exact path="/" render={() => <div>Atomic Design with Redux</div>} />
+          <Route render={() => (<div>Miss</div>)} />
+        </Switch>
+      </ConnectedRouter>
+    </AppShell>
+  </Provider>
+);
---
+++ src/store/index.ts
@@ -0,0 +1,28 @@
+import * as history from 'history';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
+import { connectRouter } from 'connected-react-router';
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: any;
+  }
+}
+
+export type InitialState = {
+  router?: any;
+}
+
+const initialState: InitialState  = {
+};
+
+const middleware: any[] = [];
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const browserHistory = history.createBrowserHistory({});
+export const store = createStore(
+  combineReducers({
+    router: connectRouter(browserHistory)
+  }),
+  initialState,
+  composeEnhancers(applyMiddleware(...middleware))
+);
