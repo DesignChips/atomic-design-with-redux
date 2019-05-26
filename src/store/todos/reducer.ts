d4243c41b592c5ab8f2329445bac94393940f8d5
Matsuda Yuichiro <Yuichiro@Matsuda-MBP.local>
2019-05-27 03:06:07 +0900

    :+1: create todos store

---
+++ src/store/todos/reducer.ts
@@ -0,0 +1,25 @@
+import { Action } from 'redux';
+import { isType } from 'typescript-fsa';
+import { addTodo, removeTodo } from './actions';
+import { Todo } from './entity';
+import { insertItem, removeItem } from '../../utils/immutable';
+
+export type TodosState = Todo[];
+
+export const todosInitialState: TodosState = [];
+
+export const todos = (
+  state: TodosState = todosInitialState,
+  action: Action
+): TodosState => {
+  if (isType(action, addTodo)) {
+    return insertItem(state, action.payload, state.length + 1);
+  }
+
+  if (isType(action, removeTodo)) {
+    const index = state.findIndex(i => i.id === action.payload.id);
+    return removeItem(state, index);
+  }
+
+  return state;
+};
