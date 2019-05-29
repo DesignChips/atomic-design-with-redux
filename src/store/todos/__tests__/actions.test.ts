7361b3f451daca42dac2688669b12149260a43f0
Matsuda Yuichiro <Yuichiro@Matsuda-MBP.local>
2019-05-30 04:41:54 +0900

    :+1: create test script

--- src/store/todos/__tests__/actions.test.ts
+++ src/store/todos/__tests__/actions.test.ts
@@ -1,6 +1,8 @@
 import {
   ADD_TODO,
+  REMOVE_TODO,
   addTodo,
+  removeTodo
 } from '../actions';
 
 describe('actions', () => {
@@ -12,5 +14,15 @@ describe('actions', () => {
       payload: { id, description }
     };
     expect(addTodo({ id, description })).toEqual(expectedAction)
-  })
+  });
+
+  it('should remove an action to remove a todo', () => {
+    const id = 1;
+    const description = 'Finish docs';
+    const expectedAction = {
+      type: REMOVE_TODO,
+      payload: { id, description }
+    };
+    expect(removeTodo({ id, description })).toEqual(expectedAction)
+  });
 });
