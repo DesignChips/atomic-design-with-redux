75a1f7fe56b94a6fa0322d6c40ed41d23fabab8d
Matsuda Yuichiro <Yuichiro@Matsuda-MBP.local>
2019-06-09 15:17:03 +0900

    :+1: implement material-ui todo list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,12 @@
 import * as React from "react";
-import Grid from '@material-ui/core/Grid';
+import { TodoList } from "../containers/TodoListContainer";
 import { TodoForm } from "../containers/TodoFormContainer";
 
 export const Index: React.FC = () => {
   return (
     <div>
-      <Grid
-        container
-        spacing={0}
-        direction="column"
-        alignItems="center"
-        justify="center"
-        style={{ minHeight: '100vh' }}
-      >
-        <Grid item style={{ width: '100%' }}>
-          <TodoForm/>
-        </Grid>
-      </Grid>
+      <TodoList/>
+      <TodoForm/>
     </div>
   )
 };
